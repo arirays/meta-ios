660f26acbaa56511c12330c92805ccd20d8a185a
arirays <arishhu@gmail.com>
2023-12-08 13:19:04 -1000

    Type casting concept

---
+++ TypeCasting.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+
+// TYPE CASTING USING "AS" "IS" 
+class Customer {
+    
+}
+
+class PeanutAllergyCustomer: Customer {
+    let dishesToAvoid = [
+    "Dish7",
+    "Dish8"
+    ]
+    
+    func executeEmergencyProcedure() {
+        print("Call 911")
+    }
+}
+
+
+var customers: [Customer] = []
+for _ in 0..<5 {
+    let randomNumber = Int.random(in: 0...1)
+    let customer: Customer
+    if randomNumber == 0 {
+        customer = Customer()
+    } else {
+        customer = PeanutAllergyCustomer()
+    }
+    customers.append(customer)
+}
+
+
+for customer in customers {
+    if customer is PeanutAllergyCustomer {
+        print("Welcome. Do note that some food has peanuts.")
+    } else {
+        print("Welcome.")
+    }
+    
+    // Downcasting use conditional form as?
+    if let peanutAllergyCustomer = customer as? PeanutAllergyCustomer {
+            print("Dishes to avoid: ")
+            print(peanutAllergyCustomer.dishesToAvoid)
+            print("Emergency Procedure:")
+            peanutAllergyCustomer.executeEmergencyProcedure()
+    }
+}
